a490cb2cfea5056919ea00090e5010cb089637d1
Mario Terron <marioterron157@gmail.com>
2017-06-06 21:58:44 +0200

    refactored code

--- client/assets/js/game.js
+++ client/assets/js/game.js
@@ -1,63 +1,16 @@
-;
-(function() {
-
-	'use strict';
-
-	var linksToModal = function() {
-
-		// Event for change a player in lineup
-		$('.to-player-modal').on('click', function(e) {
-			e.preventDefault();
-			$('#player-modal').modal('show');
-
-			$('.card-player').on('click', function() {
-				$('#player-modal').modal('hide');
-			});
-		});
-
-		// Event for make a bid
-		$('.to-bid-modal').on('click', function(e) {
-			e.preventDefault();
-			$('#bid-modal').modal('show');
-
-			$('.update-bet').on('click', function() {
-				$('#bid-modal').modal('hide');
-			});
-
-			$('.close-bet').on('click', function() {
-				$('#bid-modal').modal('hide');
-			});
-		});
-
-		// Event for update a bid
-		$('.to-update-bid-modal').on('click', function(e) {
-			e.preventDefault();
-			$('#update-bid-modal').modal('show');
-		});
-
-		// Event for check bids for a player
-		$('.to-check-bid-modal').on('click', function(e) {
-			e.preventDefault();
-			$('#check-bid-modal').modal('show');
-		});
-
-		// Event for check bids for a player
-		$('.to-update-profile-modal').on('click', function(e) {
-			e.preventDefault();
-			$('#update-profile-modal').modal('show');
-		});
-	};
-
-	var toggleProfile = function() {
-		$('.dropdown-toggle').on('click', function(e) {
-			e.preventDefault();
-			$('.top-nav > li').addClass('open');
-		});
-	}
-
-	// Document on load.
-	$(function() {
-		linksToModal();
-		toggleProfile();
+var toggleProfile = function() {
+	$('.dropdown-toggle').on('click', function(e) {
+		e.preventDefault();
+		$('.top-nav > li').addClass('open');
 	});
-}());
+}
+
+var loadTooltip = function() {
+	$('[data-toggle="tooltip"]').tooltip()
+}
+
+// Document on load.
+$(function() {
+	toggleProfile();
+	loadTooltip();
+});
